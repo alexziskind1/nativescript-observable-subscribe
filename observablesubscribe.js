e42d5de52f2f7ee2b4af9b3cc2a088bb25c4a91d
alexziskind1 <alex@nuvious.com>
2015-05-02 19:01:26 -0400

    Update observablesubscribe.js
    added unsubscribe

--- observablesubscribe.js
+++ observablesubscribe.js
@@ -1,17 +1,29 @@
 var observable = require("data/observable");
 
 var propChangeEventName = (observable.knownEvents) ? observable.knownEvents.propertyChange : observable.Observable.propertyChangeEvent;
+var subsPropName = "subscribedProperties";
 
 observable.Observable.prototype.subscribe = function(propName, callback, thisArg) {
-	
-	this.addEventListener(propChangeEventName, function(changeObj){
-		if (changeObj.propertyName == propName) {
-			if (thisArg) {
-				callback.apply(thisArg, [changeObj]);
-			}
-			else {
-				callback(changeObj);
-			}
-		}
-	}, thisArg);
+    this[subsPropName] = this[subsPropName] || [];
+    if (this[subsPropName].indexOf(propName) == -1) { //subscribe only if not already subscribed
+        this[subsPropName].push(propName);
+        this.addEventListener(propChangeEventName, function(changeObj){
+            if (changeObj.propertyName == propName && (this[subsPropName].indexOf(propName) > -1)) {
+                if (thisArg) {
+                    callback.apply(thisArg, [changeObj]);
+                }
+                else {
+                    callback(changeObj);
+                }
+            }
+        }, thisArg);
+    }
+};
+
+observable.Observable.prototype.unsubscribe = function(propName) {
+    if (typeof(this[subsPropName]) != "object") return;
+    var idx = this[subsPropName].indexOf(propName);
+    if (idx > -1) {
+        this[subsPropName].splice(idx, 1);
+    }
 };
